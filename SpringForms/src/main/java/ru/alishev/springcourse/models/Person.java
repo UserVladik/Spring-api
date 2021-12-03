383b732cd9e127d7d9a7dc701e3d5783a04b1f7e
vladik <bessmertny-vlad@mail.ru>
2021-12-03 15:39:01 +0300

    first commit

---
+++ SpringForms/src/main/java/ru/alishev/springcourse/models/Person.java
@@ -0,0 +1,61 @@
+package ru.alishev.springcourse.models;
+
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Поле не должно быть пустым")
+    @Size(min = 0 , max = 10, message = "Поле не должно быть переполненым ")
+    private String name;
+    @Min(value = 0, message = "Введите коретный возраст ")
+    private int age;
+    @NotEmpty(message = "Поле не должно быть пустым")
+    @Email
+    private String email;
+
+    public Person() {}
+
+    public Person(int id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
